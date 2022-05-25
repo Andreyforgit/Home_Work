6d4c796cc6c8178b549ed97119126b35dbf5937f
Andreyforgit <sell5432112345@gmail.com>
2022-05-25 19:38:31 +0300

    Home_Work_Project_Доработал задачу. Вынес перевод в рубли в отдельный класс

--- src/Home_Work_Project/src/Projects/ConverterTest.java
+++ src/Home_Work_Project/src/Projects/ConverterTest.java
@@ -2,19 +2,20 @@ package Projects;
 
 import org.junit.jupiter.api.Assertions;
 
-class RubleTest {
+class ConverterTest {
 
     @org.junit.jupiter.api.Test
     void digits2text() {
         String scenario = "Тест преобразования числа";
         try {
-            String actual1 = Ruble.digits2text(3.0);
+
+            String actual1 = Converter.digits2text(3.0);
             Assertions.assertEquals("Три рубля 00 копеек", actual1);
 
-            String actual2 = Ruble.digits2text(33.0);
+            String actual2 = Converter.digits2text(33.0);
             Assertions.assertEquals("Тридцать три рубля 00 копеек", actual2);
 
-            String actual3 = Ruble.digits2text(333.0);
+            String actual3 = Converter.digits2text(333.0);
             Assertions.assertEquals("Триста тридцать три рубля 00 копеек", actual3);
 
             System.out.printf("\"%s\" passed %n", scenario);
--- src/Home_Work_Project/src/Projects/Main.java
+++ src/Home_Work_Project/src/Projects/Main.java
@@ -8,6 +8,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int number = Integer.parseInt(reader.readLine());
-        System.out.println(Ruble.digits2text((double) number));
+        //System.out.println(new Ruble().addRub((double) number, Converter.digits2text((double) number)));
+
+        convert(number, new Ruble());
+
+
+
+    }
+
+    static String convert(int digit, Currency currency){
+        System.out.println(currency.addRub((double) digit, Converter.digits2text((double) digit)));
+        return null;
     }
 }
